eb5ed6f6acc3b387ef8c63c9a1dfff25834207fc
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2020-10-19 11:24:58 +0200

    Add Tournament Viewer window

---
+++ TrackerUI/TournamentTrackerWPF.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class TournamentViewer : Window
+    {
+        public TournamentViewer()
+        {
+            InitializeComponent();
+
+            if(this.scoreButton.IsMouseOver && System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                scoreButton.Background = new SolidColorBrush(Color.FromRgb(102, 102, 102));
+            }
+            else if(this.scoreButton.IsMouseOver && System.Windows.Input.Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                scoreButton.Background = new SolidColorBrush(Color.FromRgb(242, 242, 242));
+            }
+        }
+    }
+}
